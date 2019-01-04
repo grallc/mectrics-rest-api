d3630d3a970d9e95f28363709d393d944ed90078
Corentin Grall <corentin.grall@gmail.com>
2019-01-04 13:33:31 +0100

    Started Handling / :construction:

--- main.go
+++ main.go
@@ -5,8 +5,13 @@ import (
 	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	w.Write([]byte(`{"message": "hello world"}`))
+}
+
 func main() {
 	fmt.Println("Starting server on port 8001...")
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8001", nil)
-
 }
