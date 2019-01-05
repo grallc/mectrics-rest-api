7cb6727bf8ee31cd67b5f14e0cb1fc788d738a11
Corentin Grall <corentin.grall@gmail.com>
2019-01-05 12:01:13 +0100

    Added GET and POST routes :construction:

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// GetConnectionsEndpoint - GET all connections
+func GetConnectionsEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	w.Write([]byte(`{"message": "hello world"}`))
 }
 
+// AddConnectionsEndpoint - POST a new connection
+func AddConnectionsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "not implemented yet !")
+}
+
 func main() {
 	fmt.Println("Starting server on port 8001...")
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8001", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/metrics", GetConnectionsEndpoint).Methods("GET")
+	r.HandleFunc("/metrics", AddConnectionsEndpoint).Methods("POST")
+	if err := http.ListenAndServe(":8001", r); err != nil {
+		log.Fatal(err)
+	}
 }
