788702f8cd6f6f55e468ccff9a6d9f1ac7b76291
Corentin Grall <corentin.grall@gmail.com>
2019-01-05 12:54:46 +0100

    :sparkles::construction::heavy_plus_sign: Started adding MongoDB

--- main.go
+++ main.go
@@ -6,16 +6,23 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Connection - Model (struct)
 type Connection struct {
-	ID          string  `json:"id"`
-	ServerURL   string  `json:"server"`
-	InitialTime float64 `json:"initial"`
-	FinalTime   float64 `json:"final"`
-	Duration    float64 `json:"duration"`
+	ID          bson.ObjectId `bson:"_id" json:"id"`
+	ServerURL   string        `json:"server"`
+	InitialTime float64       `json:"initial"`
+	FinalTime   float64       `json:"final"`
+	Duration    float64       `json:"duration"`
 }
+
+var (
+	session    *mgo.Session
+	collection *mgo.Collection
+)
 
 // GetConnectionsEndpoint - GET all connections
 func GetConnectionsEndpoint(w http.ResponseWriter, r *http.Request) {
@@ -29,10 +36,22 @@ func AddConnectionsEndpoint(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fmt.Println("Starting server on port 8001...")
+	log.Println("Starting server on port 8001...")
 	r := mux.NewRouter()
 	r.HandleFunc("/metrics", GetConnectionsEndpoint).Methods("GET")
 	r.HandleFunc("/metrics", AddConnectionsEndpoint).Methods("POST")
+
+	log.Println("Starting MongoDB session")
+	var err error
+	session, err = mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+	collection = session.DB("connectionsdb").C("connections")
+
 	if err := http.ListenAndServe(":8001", r); err != nil {
 		log.Fatal(err)
 	}
